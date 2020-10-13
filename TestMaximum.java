99a1e33ad3b5e53197ac61b46216114ac609f236
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-10-13 10:17:24 +0530

    [abhishek] Refactor1: refactor all the 3 to one generic method aand find maximum

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,66 +1,24 @@
 package com.bridgelabz.problemDay13;
 
 public class TestMaximum {
-	/**
-	 * uc1
-	 * 
-	 * @param a
-	 * @param b
-	 * @param c
-	 * @return
-	 */
-	public static Integer maximumNumber_WhenInput_IsInteger(Integer a, Integer b, Integer c) {
-		Integer max = 0;
+	public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
+		T max;
 		if (a.compareTo(b) > 0)
 			max = a;
 		else
 			max = b;
 		if (max.compareTo(c) < 0)
 			max = c;
+
 		return max;
 	}
 
-	/**
-	 * uc2
-	 * 
-	 * @param a
-	 * @param b
-	 * @param c
-	 * @return
-	 */
-	public static Float maximumNumber_WhenInput_IsFloat(Float a, Float b, Float c) {
-		Float max;
-		if (a.compareTo(b) > 0)
-			max = a;
-		else
-			max = b;
-		if (max.compareTo(c) < 0)
-			max = c;
-		return max;
-	}
-	/**
-	 * uc3
-	 * @param a
-	 * @param b
-	 * @param c
-	 * @return
-	 */
-	public static String stringWhocome_Last_Inlexicographically(String a, String b, String c) {
-		String max;
-		if (a.compareTo(b) > 0)
-			max = a;
-		else
-			max = b;
-		if (max.compareTo(c) < 0)
-			max = c;
-		return max;
-	}
 	public static void main(String[] args) {
 		Integer a = 45, b = 15, c = 35;
 		Float firstNumber = 110f, secondNumber = 98f, thirdNumber = 87f;
-		String firstString="abhishek",secondString="rahul",thirdString="abhineet";
-		System.out.println(maximumNumber_WhenInput_IsInteger(a, b, c));
-		System.out.println(maximumNumber_WhenInput_IsFloat(firstNumber, secondNumber, thirdNumber));
-		System.out.println(stringWhocome_Last_Inlexicographically(firstString,secondString,thirdString));
+		String firstString = "abhishek", secondString = "rahul", thirdString = "abhineet";
+		System.out.println(testMaximum(a, b, c));
+		System.out.println(testMaximum(firstNumber, secondNumber, thirdNumber));
+		System.out.println(testMaximum(firstString, secondString, thirdString));
 	}
 }
--- TestMaximumTest.java
+++ TestMaximumTest.java
@@ -1,8 +1,6 @@
 package com.example.demo;
-
 import org.junit.Assert;
 import org.junit.Test;
-
 import com.bridgelabz.problemDay13.TestMaximum;
 
 public class TestMaximumTest 
@@ -12,27 +10,27 @@ public class TestMaximumTest
 		 Integer a=23,b=9,c=12;
 		 Integer d=32,e=67,f=9;
 		 Integer k=7,l=76,m=98;
-		 Assert.assertEquals(a,TestMaximum. maximumNumber_WhenInput_IsInteger(a,b,c));
-		 Assert.assertEquals(e,TestMaximum. maximumNumber_WhenInput_IsInteger(d,e,f));
-		 Assert.assertEquals(m,TestMaximum. maximumNumber_WhenInput_IsInteger(k,l,m));
+		 Assert.assertEquals(a,TestMaximum.testMaximum(a,b,c));
+		 Assert.assertEquals(e,TestMaximum. testMaximum(d,e,f));
+		 Assert.assertEquals(m,TestMaximum. testMaximum(k,l,m));
 	}
 	@Test
 	public void maximumNumber_WhenInputIsFloat() {
 		 Float a=23f,b=9f,c=12f;
 		 Float d=32f,e=67f,f=9f;
 		 Float k=7f,l=76f,m=98f;
-		 Assert.assertEquals(a,TestMaximum.maximumNumber_WhenInput_IsFloat(a,b,c));
-		 Assert.assertEquals(e,TestMaximum.maximumNumber_WhenInput_IsFloat(d,e,f));
-		 Assert.assertEquals(m,TestMaximum.maximumNumber_WhenInput_IsFloat(k,l,m));
+		 Assert.assertEquals(a,TestMaximum.testMaximum(a,b,c));
+		 Assert.assertEquals(e,TestMaximum.testMaximum(d,e,f));
+		 Assert.assertEquals(m,TestMaximum.testMaximum(k,l,m));
 	}
 	@Test
 	public void stringWhocome_Last_Inlexicographically_WhenInput_IsFloat() {
 		 String a="dhoni",b="abhi",c="bhagat";
 		 String d="naman",e="sashi",f="abhishek";
 		 String k="bhagat",l="abhishek",m="sashi";
-		 Assert.assertEquals(a,TestMaximum.stringWhocome_Last_Inlexicographically(a,b,c));
-		 Assert.assertEquals(e,TestMaximum.stringWhocome_Last_Inlexicographically(d,e,f));
-		 Assert.assertEquals(m,TestMaximum.stringWhocome_Last_Inlexicographically(k,l,m));
+		 Assert.assertEquals(a,TestMaximum.testMaximum(a,b,c));
+		 Assert.assertEquals(e,TestMaximum.testMaximum(d,e,f));
+		 Assert.assertEquals(m,TestMaximum.testMaximum(k,l,m));
 	}
 	
 }
