8c706109255f20ed56c276e7d1e92be80967b8d7
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-10-13 11:18:28 +0530

    [abhishek] refactor: create generci class to take in 3 variables

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,24 +1,34 @@
 package com.bridgelabz.problemDay13;
 
-public class TestMaximum {
-	public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
-		T max;
-		if (a.compareTo(b) > 0)
-			max = a;
+public  class TestMaximum<T extends Comparable<T>> {
+	private T firstNumber;
+	private T secondNumber;
+	private T thirdNumber;
+	
+	public TestMaximum (T fistNumber, T secondNumber, T thirdNumber) {
+		super();
+		this.firstNumber = fistNumber;
+		this.secondNumber = secondNumber;
+		this.thirdNumber = thirdNumber;
+	}
+	public <T extends Comparable<T>> T testMaximum() {
+		T max ;
+		if (firstNumber.compareTo(secondNumber) > 0)
+			max = (T) firstNumber;
 		else
-			max = b;
-		if (max.compareTo(c) < 0)
-			max = c;
+			max = (T) secondNumber;
+		if (max.compareTo((T) thirdNumber) < 0)
+			max = (T) thirdNumber;
 
 		return max;
 	}
 
 	public static void main(String[] args) {
-		Integer a = 45, b = 15, c = 35;
+		Integer a = 80, b = 15, c = 35;
 		Float firstNumber = 110f, secondNumber = 98f, thirdNumber = 87f;
 		String firstString = "abhishek", secondString = "rahul", thirdString = "abhineet";
-		System.out.println(testMaximum(a, b, c));
-		System.out.println(testMaximum(firstNumber, secondNumber, thirdNumber));
-		System.out.println(testMaximum(firstString, secondString, thirdString));
+		System.out.println(new TestMaximum(a, b, c).testMaximum());
+		System.out.println(new TestMaximum(firstNumber, secondNumber, thirdNumber).testMaximum());
+		System.out.println(new TestMaximum(firstString, secondString, thirdString).testMaximum());
 	}
 }
--- TestMaximumTest.java
+++ TestMaximumTest.java
@@ -1,6 +1,8 @@
 package com.example.demo;
+
 import org.junit.Assert;
 import org.junit.Test;
+
 import com.bridgelabz.problemDay13.TestMaximum;
 
 public class TestMaximumTest 
@@ -10,27 +12,31 @@ public class TestMaximumTest
 		 Integer a=23,b=9,c=12;
 		 Integer d=32,e=67,f=9;
 		 Integer k=7,l=76,m=98;
-		 Assert.assertEquals(a,TestMaximum.testMaximum(a,b,c));
-		 Assert.assertEquals(e,TestMaximum. testMaximum(d,e,f));
-		 Assert.assertEquals(m,TestMaximum. testMaximum(k,l,m));
+		 Assert.assertEquals(a,new TestMaximum(a, b, c).testMaximum());
+		 Assert.assertEquals(e,new TestMaximum(d,e,f).testMaximum());
+		 Assert.assertEquals(m,new TestMaximum(k, l, m).testMaximum());
 	}
 	@Test
 	public void maximumNumber_WhenInputIsFloat() {
+
 		 Float a=23f,b=9f,c=12f;
 		 Float d=32f,e=67f,f=9f;
 		 Float k=7f,l=76f,m=98f;
-		 Assert.assertEquals(a,TestMaximum.testMaximum(a,b,c));
-		 Assert.assertEquals(e,TestMaximum.testMaximum(d,e,f));
-		 Assert.assertEquals(m,TestMaximum.testMaximum(k,l,m));
+
+		 Assert.assertEquals(a,new TestMaximum(a, b, c).testMaximum());
+		 Assert.assertEquals(e,new TestMaximum(d,e,f).testMaximum());
+		 Assert.assertEquals(m,new TestMaximum(k, l, m).testMaximum());
 	}
 	@Test
-	public void stringWhocome_Last_Inlexicographically_WhenInput_IsFloat() {
+	public void stringWhocome_Last_Inlexicographically_WhenInput_IsString() {
+
 		 String a="dhoni",b="abhi",c="bhagat";
 		 String d="naman",e="sashi",f="abhishek";
 		 String k="bhagat",l="abhishek",m="sashi";
-		 Assert.assertEquals(a,TestMaximum.testMaximum(a,b,c));
-		 Assert.assertEquals(e,TestMaximum.testMaximum(d,e,f));
-		 Assert.assertEquals(m,TestMaximum.testMaximum(k,l,m));
+
+		 Assert.assertEquals(a,new TestMaximum(a, b, c).testMaximum());
+		 Assert.assertEquals(e,new TestMaximum(d,e,f).testMaximum());
+		 Assert.assertEquals(m,new TestMaximum(k, l, m).testMaximum());
 	}
 	
 }
