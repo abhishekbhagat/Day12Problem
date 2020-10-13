b3d343563cb72668d49523d8d31b333d8b2e9747
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-10-13 09:37:23 +0530

    [abhishek] add: given 3 floats find the maximum

--- TestMaximum.java
+++ TestMaximum.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.problemDay13;
 
 public class TestMaximum {
-	public  static Integer maximumNumber(Integer a, Integer b, Integer c) {
+	/**
+	 * uc1
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return
+	 */
+	public static Integer maximumNumber_WhenInput_IsInteger(Integer a, Integer b, Integer c) {
 		Integer max = 0;
 		if (a.compareTo(b) > 0)
 			max = a;
@@ -9,16 +17,32 @@ public class TestMaximum {
 			max = b;
 		if (max.compareTo(c) < 0)
 			max = c;
-		printMax(max);
 		return max;
 	}
 
-	public static void printMax(Integer max) {
-		System.out.println("Maximum number =  " + max);
+	/**
+	 * uc2
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return
+	 */
+	public static Float maximumNumber_WhenInput_IsFloat(Float a, Float b, Float c) {
+		Float max;
+		if (a.compareTo(b) > 0)
+			max = a;
+		else
+			max = b;
+		if (max.compareTo(c) < 0)
+			max = c;
+		return max;
 	}
 
 	public static void main(String[] args) {
 		Integer a = 45, b = 15, c = 35;
-		maximumNumber(a, b, c);
+		Float firstNumber = 110f, secondNumber = 98f, thirdNumber = 87f;
+		System.out.println(maximumNumber_WhenInput_IsInteger(a, b, c));
+		System.out.println(maximumNumber_WhenInput_IsFloat(firstNumber, secondNumber, thirdNumber));
 	}
 }
--- TestMaximumTest.java
+++ TestMaximumTest.java
@@ -9,14 +9,23 @@ public class TestMaximumTest
 {
 	@Test
 	public void maximumNumber_WhenInputIsInteger() {
-		 TestMaximum testMaximum=new TestMaximum();
 		 Integer a=23,b=9,c=12;
 		 Integer d=32,e=67,f=9;
 		 Integer k=7,l=76,m=98;
-		 testMaximum.maximumNumber(a,b,c);
-		 Assert.assertEquals(a,testMaximum.maximumNumber(a,b,c));
-		 Assert.assertEquals(e,testMaximum.maximumNumber(d,e,f));
-		 Assert.assertEquals(m,testMaximum.maximumNumber(k,l,m));
+		 Assert.assertEquals(a,TestMaximum. maximumNumber_WhenInput_IsInteger(a,b,c));
+		 Assert.assertEquals(e,TestMaximum. maximumNumber_WhenInput_IsInteger(d,e,f));
+		 Assert.assertEquals(m,TestMaximum. maximumNumber_WhenInput_IsInteger(k,l,m));
+	}
+	@Test
+	public void maximumNumber_WhenInputIsFloat() {
+
+		 Float a=23f,b=9f,c=12f;
+		 Float d=32f,e=67f,f=9f;
+		 Float k=7f,l=76f,m=98f;
+
+		 Assert.assertEquals(a,TestMaximum.maximumNumber_WhenInput_IsFloat(a,b,c));
+		 Assert.assertEquals(e,TestMaximum.maximumNumber_WhenInput_IsFloat(d,e,f));
+		 Assert.assertEquals(m,TestMaximum.maximumNumber_WhenInput_IsFloat(k,l,m));
 	}
 	
 }
